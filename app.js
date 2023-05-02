2f65b3445a6bf84743593ddba23204af69fb4cf3
Anmitchell <avarynmitchell@gmail.com>
2023-05-01 21:53:02 -0400

    Added Player Movement

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// *****Building out the game grid*****
+/*----- cached Elements -----*/
 
 const grid = document.querySelector('.grid') // select the element that has a class of grid
 
-// Create grid layout
+// Grid layout
 for (let i = 0; i < 380; i++) {
-    const gridSquare = document.createElement('div')
-    grid.appendChild(gridSquare) 
+    const divEl = document.createElement('div')
+    grid.appendChild(divEl) 
 }
 
-// *****Placing invaders on grid*****
+/*----- Invaders -----*/
 
-const gridSquares = Array.from(document.querySelectorAll('.grid div')) // select all divs in the grid
+const gridElements = Array.from(document.querySelectorAll('.grid div'))
 
 // Invaders positions on grid
 const invaders = [
@@ -21,17 +21,79 @@ const invaders = [
     117,119,121,123,125,127,129
 ]
 
-// Add invaders to DOM
-function draw() {
+// Placing invaders in grid
+function renderInvaders() {
     for (let i = 0; i < invaders.length; i++) {
-        gridSquares[invaders[i]].classList.add('invader') // add invader class to specific divs in grid
+        gridElements[invaders[i]].classList.add('invader') // add invader class to specific divs in grid
     }
 }
 
-draw()
+// needed for invader movement
+function remove () {
+    for (let i = 0; i < invaders.length; i++) {
+        gridElements[i].classList.remove('invader')
+    }
+}
 
-// *****Player position on grid*****
-playerPosition = 332;
+// Invader Movement
 
-gridSquares[playerPosition].classList.add('player')
+function invaderMovement() {
+    const leftEdge = invaders[39] % width === 0
+    const rightEdge = invaders[invaders.length - 1] % width === width -1
+    remove()
+  
+    if (rightEdge && goingRight) {
+      for (let i = 0; i < invaders.length; i++) {
+        invaders[i] += width + 1
+        direction = -1
+        goingRight = false
+      }
+    }
+  
+    if(leftEdge && !goingRight) {
+      for (let i = 0; i < invaders.length; i++) {
+        invaders[i] += width - 1
+        direction = 1
+        goingRight = true
+      }
+    }
+  
+    for (let i = 0; i < alienInvaders.length; i++) {
+      alienInvaders[i] += direction
+    }
+}
+
+renderInvaders() // Display invaders
+let movementSpeed = setInterval(invaderMovement, 500)
+
+/*----- Player position on grid -----*/
+
+
+playerPosition = 332; // player starting position
+const gridWidth = 19; // used to bound check the player on the left and right edges of the grid
+
+gridElements[playerPosition].classList.add('player')
+
+
+/*----- Controls for player -----*/
+
+function movePlayer(event) {
+    const leftEdge = 0;
+    const rightEdge = gridWidth - 1;
+    gridElements[playerPosition].classList.remove('player')
+    if (event.key === 'ArrowLeft') {
+        if (playerPosition % gridWidth !== leftEdge) playerPosition -= 1
+    }
+    if (event.key === 'ArrowRight') {
+        if (playerPosition % gridWidth < rightEdge ) playerPosition += 1
+    }
+    gridElements[playerPosition].classList.add('player')
+}
+
+/*----- Event Listeners -----*/
+
+// Event for player moving left and right
+document.addEventListener('keydown',movePlayer)
+
+// Event for player shooting
 
