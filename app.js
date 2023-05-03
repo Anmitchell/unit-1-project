595007196f4dfd1e3a9287f2713f7e8efcc8bf2e
Anmitchell <avarynmitchell@gmail.com>
2023-05-03 02:30:37 -0400

    Created Player, &  Added Controls and Movement

--- app.js
+++ app.js
@@ -1,99 +1,151 @@
-/*----- cached Elements -----*/
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d') // api object
 
-const grid = document.querySelector('.grid') // select the element that has a class of grid
+canvas.width = innerWidth  // window.innerWidth
+canvas.height = innerHeight // window.innerHeight
 
-// Grid layout
-for (let i = 0; i < 380; i++) {
-    const divEl = document.createElement('div')
-    grid.appendChild(divEl) 
-}
-
-/*----- Invaders -----*/
-
-const gridElements = Array.from(document.querySelectorAll('.grid div'))
-
-// Invaders positions on grid
-const invaders = [
-    41,43,45,47,49,51,53,
-    60,62,64,66,68,70,72,
-    79,81,83,85,87,89,91,
-    98,100,102,104,106,108,110,
-    117,119,121,123,125,127,129
-]
-
-// Placing invaders in grid
-function renderInvaders() {
-    for (let i = 0; i < invaders.length; i++) {
-        gridElements[invaders[i]].classList.add('invader') // add invader class to specific divs in grid
+/*------Creating Player and Setting Position------*/
+class Player {
+  constructor() {
+    this.position = {
+      x: 0,
+      y: 0
     }
-}
-
-// needed for invader movement
-function remove () {
-    for (let i = 0; i < invaders.length; i++) {
-        gridElements[i].classList.remove('invader')
+    
+    this.velocity = {
+      x: 0,
+      y: 0
     }
-}
 
-// Invader Movement
+    this.rotation = 0
 
-function invaderMovement() {
-    const leftEdge = invaders[39] % width === 0
-    const rightEdge = invaders[invaders.length - 1] % width === width -1
-    remove()
-  
-    if (rightEdge && goingRight) {
-      for (let i = 0; i < invaders.length; i++) {
-        invaders[i] += width + 1
-        direction = -1
-        goingRight = false
+    // Image for player icon
+    const image = new Image() // image object from javascript API
+    image.src = 'img/25129-4-spaceship-file.png'; // image used
+
+    // listen for when image has fully loaded in canvas
+    // image does not fully load before calling player.draw()
+    // onload is needed to ensure that image is loaded before 
+    // player.draw() method
+
+    // initially load player in bottom center of screen and 
+    // set image width and height to scale of screen size
+    // Tells DOM to load image before rest of DOM elements?
+    image.onload = () => { //
+      const scale = .10 // multiplier for scaling
+      this.image = image
+      this.width = image.width * scale 
+      this.height = image.height * scale
+      // Place player in middle of canvas
+      this.position = {
+        x: canvas.width / 2 - this.width / 2,
+        y: canvas.height - this.height - 20
       }
     }
-  
-    if(leftEdge && !goingRight) {
-      for (let i = 0; i < invaders.length; i++) {
-        invaders[i] += width - 1
-        direction = 1
-        goingRight = true
-      }
+  }
+
+  // render image of player on canvas
+  render() {
+    ctx.drawImage( // draws image from instance of image object
+      this.image, 
+      this.position.x, 
+      this.position.y, 
+      this.width, 
+      this.height
+    )
+  }
+
+  update() {
+    if (this.image) {
+      this.render()
+      this.position.x += this.velocity.x
     }
-  
-    for (let i = 0; i < alienInvaders.length; i++) {
-      alienInvaders[i] += direction
-    }
+  }
 }
 
-renderInvaders() // Display invaders
-let movementSpeed = setInterval(invaderMovement, 500)
+/*------Player Controls For Moving------*/
 
-/*----- Player position on grid -----*/
-
-
-playerPosition = 332; // player starting position
-const gridWidth = 19; // used to bound check the player on the left and right edges of the grid
-
-gridElements[playerPosition].classList.add('player')
-
-
-/*----- Controls for player -----*/
-
-function movePlayer(event) {
-    const leftEdge = 0;
-    const rightEdge = gridWidth - 1;
-    gridElements[playerPosition].classList.remove('player')
-    if (event.key === 'ArrowLeft') {
-        if (playerPosition % gridWidth !== leftEdge) playerPosition -= 1
-    }
-    if (event.key === 'ArrowRight') {
-        if (playerPosition % gridWidth < rightEdge ) playerPosition += 1
-    }
-    gridElements[playerPosition].classList.add('player')
+// Used to monitor keys used for player controls and give smoother
+// control when moving left to right
+const keys = {
+  ArrowLeft: {
+    pressed: false
+  },
+  ArrowRight: {
+    pressed: false
+  },
+  spacebar: {
+    pressed: false
+  }
 }
 
-/*----- Event Listeners -----*/
+addEventListener('keydown', ({key}) => { // {key} === event.key
+  const spacebar = ' '
+  switch (key) {
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = true
+      break
+    case 'ArrowRight':
+      keys.ArrowRight.pressed = true
+      break
+    case spacebar:
+      console.log('spacebar') // player will shoot here
+  }
+})
 
-// Event for player moving left and right
-document.addEventListener('keydown',movePlayer)
+addEventListener('keyup', ({key}) => { // {key} === event.key
+  const spacebar = ' '
+  switch (key) {
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = false
+      break
+    case 'ArrowRight':
+      keys.ArrowRight.pressed = false
+      break
+    case spacebar:
+      console.log('spacebar') // player will shoot here
+  }
+})
 
-// Event for player shooting
+/*------Rendering Player on Webpage------*/
+const player = new Player()
 
+// Loop to render player
+function animate() {
+  requestAnimationFrame(animate) // loop animate function
+
+  // Default Background color for image
+  ctx.fillStyle = 'black'
+  // 
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+  // Bound Checking and Moving player
+  const leftEdge = 0;
+  const rightEdge = canvas.width
+  const speed = 4
+
+  if(keys.ArrowLeft.pressed && player.position.x > leftEdge) {
+    player.velocity.x = speed * -1 // left speed -
+    // rotate player here
+  } 
+  else if (keys.ArrowRight.pressed && player.position.x + player.width < rightEdge){
+    player.velocity.x = speed // right speed +
+    // rotate player here
+  }
+  else {
+    player.velocity.x = 0
+  }
+
+  player.update()
+}
+
+animate()
+
+/*
+  Notes:
+  * Find a way to optimise and clean code a bit better
+  * How can I get image to load with DOM content in a cleaner way?
+  * How to use CSS animation to rotate player to seemingly tilt when shooting
+  * How to remove image and place a CSS styled element with animations in its place
+  * 
+*/
